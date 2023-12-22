cc9a34538725f1fe5ab9f905a348efee04ba9f56
rydergallagher <rgallagher03@yahoo.com>
2023-12-22 13:02:03 -0800

    added basic inline comments to give a general sense of what the code is doing

--- cube.cpp
+++ cube.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 int main()
 {
-    ios_base ::sync_with_stdio(false), cin.tie(NULL);
-    ofstream file;
-    file.open("cube.txt");
+    ios_base ::sync_with_stdio(false), cin.tie(NULL); // unties the c++ and c stream operators (stdio), unties cin and cout operators
+    // these changes make cin and stream operators perform faster
+    ofstream file; // opens an output file stream
+    file.open("cube.txt"); // opens the file "cube.txt" which holds a single string, "cube" to be outputted from
+
+    //CODE NOT IN USE
+
     /*for (int i = 1; i <= 9; i++)
     {
         int x, y, z;
@@ -41,14 +45,16 @@ int main()
         cin >> x >> y >> z;
         file << "#d" << i << "{\ntransform : rotateX(90deg) translate3d(" << x << "px," << y << "px," << z << "px);}\n";
     }*/
+    
     int r = 23;
-    for (int i = r; i <= r + 1; i++)
+    for (int i = r; i <= r + 1; i++) // iterates twice
     {
         int n;
         cin >> n;
         string cur;
-        for (int k = 0; k < n; k++)
+        for (int k = 0; k < n; k++) // iterates n times, dictated by cin
         {
+            // creating a css transformation
             char c;
             string a;
             cin >> c >> a;
@@ -60,6 +66,7 @@ int main()
         }
         for (int j = 1; j <= 4; j++)
         {
+            // writing css transformations to the output file
             file << ".s" << i << j << "{\n  animation-name : s" << i << j << ";\n}\n@keyframes s" << i << j << "{\n";
 
             file << "from{\n  transform : var(--dt) ";
@@ -70,11 +77,11 @@ int main()
             int a;
             cin >> c >> a;
             c = toupper(c);
-            file << "rotate" << c << "(" << a << "deg) ";
+            file << "rotate" << c << "(" << a << "deg) "; 
             file << ";\n}\n}\n";
         }
     }
 
-    file.close();
+    file.close(); // closes the output file stream
     return 0;
 }
